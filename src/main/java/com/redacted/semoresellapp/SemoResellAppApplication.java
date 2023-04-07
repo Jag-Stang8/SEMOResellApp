21dad6cb89ea8fe2d37c26bf6555ab4c5e4a5eb0
Jag-Stang8 <seth.hatchel@gmail.com>
2023-04-07 13:40:22 -0500

    Initial commit

---
+++ src/main/java/com/redacted/semoresellapp/SemoResellAppApplication.java
@@ -0,0 +1,13 @@
+package com.redacted.semoresellapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoResellAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemoResellAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redacted/semoresellapp/SemoResellAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redacted.semoresellapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoResellAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
